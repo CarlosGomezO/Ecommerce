7a885cc7727041e6217cf6a4da3eb816897f44b7
Carlos Gomez <carlosmgo94@gmail.com>
2022-11-24 22:54:12 -0300

    Ecom1

---
+++ django_mysql/db.py
@@ -0,0 +1,9 @@
+MYSQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-mysql',
+        'USER': 'root',
+        'PASSWORD': 'Cgomez30',
+        'PORT': '3306'
+    }
+}
